37f205858b2384d857111884e9fa277c2b673fa1
Sean Trent <sean.d.trent@gmail.com>
2020-02-10 16:17:22 -0500

    Completed 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,39 @@
-#write your code here
+def echo(word)
+    return "#{word}"
+end
+
+def shout(word)
+    return "#{word}".upcase
+end
+
+def repeat(word, num_repeats = 2)
+    string = "#{word} " * num_repeats
+    return string.chomp(' ')
+end
+
+def start_of_word(word, range)
+    return word[0..range-1]
+end
+
+def first_word(sentence)
+    words = sentence.split
+    return words[0]
+end
+
+def titleize(sentence)
+    words = sentence.split
+    x = 0
+    while x < words.length
+        if x == 0
+            words[x] = words[x].capitalize
+        else
+            if (words[x].downcase != "and" && words[x].downcase != "of" && words[x].downcase != "the" && words[x].downcase != "over")
+                words[x] = words[x].capitalize
+            end
+        end
+        x += 1
+    end
+
+    return words * " "
+
+end
