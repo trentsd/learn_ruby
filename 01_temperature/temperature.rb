ccb3e4b32c0d75d2ae3c286b749ce57e5a62b73f
Sean Trent <sean.d.trent@gmail.com>
2020-02-10 15:19:28 -0500

    Completed 01_temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
-#write your code here
+def ftoc(temp)
+    temp = temp.to_f
+    celcius = (temp - 32) * (5.0/9.0)
+    return celcius
+end
+
+def ctof(temp)
+    temp = temp.to_f
+    fahrenheit = temp * 9.0/5.0 + 32
+    return fahrenheit
+end
