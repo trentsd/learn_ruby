d1b78eaed5a5da7033cae3bfda99f009beeaa5a3
Sean Trent <sean.d.trent@gmail.com>
2020-02-10 15:26:05 -0500

    Completed 02_calculator basic requirements

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,15 @@
-#write your code here
+def add(a, b)
+    return a + b
+end
+
+def subtract(a, b)
+    return a - b
+end
+
+def sum(numbers)
+    total = 0
+    numbers.each do |num|
+        total = total + num
+    end
+    return total
+end
