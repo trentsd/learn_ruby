5a0a203f12c03d42740cbab948aa89ebc7495d93
Sean Trent <sean.d.trent@gmail.com>
2020-02-10 17:56:05 -0500

    Completed 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds, :time_string
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    left = @seconds
+    hours_place = left / 60**2
+    left = left % 60**2
+    minutes_place = left / 60
+    seconds_palce = left % 60
+    @time_string = "#{hours_place.to_s.rjust(2,'0')}:#{minutes_place.to_s.rjust(2,'0')}:#{seconds_palce.to_s.rjust(2,'0')}"
+  end
 end
