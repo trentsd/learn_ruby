6665cb4168e50ad242ab9fbb4c3923eee8f9f5ba
Sean Trent <sean.d.trent@gmail.com>
2020-02-10 17:31:05 -0500

    Completed 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+    attr_accessor :title
+
+    def title=(sentence)
+        bad_words = ["and", "of", "the", "over", "in", "a", "an"]
+        words = sentence.split
+        x = 0
+        while x < words.length
+            if x == 0
+                words[x] = words[x].capitalize
+            else
+                if (!bad_words.include?(words[x].downcase))
+                    words[x] = words[x].capitalize
+                end
+            end
+            x += 1
+        end
+        @title = words * " "
+    end
 end
